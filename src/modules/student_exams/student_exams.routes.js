778a658c0011978b91078c1b8b9576708dac3035
benben-prog <benben@benb3n.com>
2026-08-30 04:34:36 +0300

    Update student_exams.routes.js

--- src/modules/student_exams/student_exams.routes.js
+++ src/modules/student_exams/student_exams.routes.js
@@ -2,11 +2,38 @@ const express = require("express");
 const routes = express.Router();
 const studentExamController = require("./student_exams.controller");
 
-// Start exam (student)
+// Check active attempt
+routes.get(
+  "/exam/:examId/check-attempt",
+  studentExamController.checkActiveAttempt,
+);
+
+// Resume exam
+routes.get("/exam/:examId/resume", studentExamController.resumeExam);
+
+// Start exam
 routes.post("/exam/:examId/start", studentExamController.createExamAttempt);
 
-// Submit exam (student)
+// Submit exam
 routes.put("/attempt/:attemptId/submit", studentExamController.submitExam);
+
+// Get exam questions for student
+routes.get(
+  "/exam/:examId/questions",
+  studentExamController.getExamQuestionsForStudent,
+);
+
+// Get single question for student
+routes.get(
+  "/question/:questionId",
+  studentExamController.getQuestionForStudent,
+);
+
+// Get options for student
+routes.get(
+  "/options/question/:questionId",
+  studentExamController.getOptionsForStudent,
+);
 
 // Get students by exam
 routes.get("/exam/:examId", studentExamController.getStudentExamsByExamId);
@@ -15,9 +42,15 @@ routes.get("/exam/:examId", studentExamController.getStudentExamsByExamId);
 routes.get("/exam/:examId/stats", studentExamController.getExamAttemptStats);
 
 // Get grade stats
-routes.get("/grade/:gradeId/stats", studentExamController.getGradeExamAttemptsStats);
+routes.get(
+  "/grade/:gradeId/stats",
+  studentExamController.getGradeExamAttemptsStats,
+);
 
 // Get group stats
-routes.get("/group/:groupId/stats", studentExamController.getGroupExamAttemptsStats);
+routes.get(
+  "/group/:groupId/stats",
+  studentExamController.getGroupExamAttemptsStats,
+);
 
 module.exports = routes;
