a58adb9cb3ed7f1cf1e0ffda71be74637d0248d6
benben-prog <benben@benb3n.com>
2026-08-30 23:37:54 +0300

    Update assignment.validation.js

--- src/middlewares/validations/assignment.validation.js
+++ src/middlewares/validations/assignment.validation.js
@@ -1,4 +1,6 @@
 const Joi = require("joi");
+
+// Create assignment
 const createAssignmentSchema = Joi.object({
   title: Joi.string().required().min(3).max(255),
   description: Joi.string().allow("", null).max(2000),
@@ -7,20 +9,22 @@ const createAssignmentSchema = Joi.object({
   full_mark: Joi.number().required().min(1).max(999),
   deadline: Joi.date().iso().required(),
   file_path: Joi.string().allow("", null).max(255),
+  is_closed: Joi.number().integer().valid(0, 1).default(0),
 });
 
+// Update assignment
 const updateAssignmentSchema = Joi.object({
   title: Joi.string().min(3).max(255),
   description: Joi.string().allow("", null).max(2000),
   grade_id: Joi.number().integer(),
   group_id: Joi.number().integer().allow(null),
-  filePath: Joi.string().allow("", null).max(255),
-  fullMark: Joi.number().min(1).max(999),
+  file_path: Joi.string().allow("", null).max(255),
+  full_mark: Joi.number().min(1).max(999),
   deadline: Joi.date().iso(),
-  isClosed: Joi.number().integer().valid(0, 1)
-});
+  is_closed: Joi.number().integer().valid(0, 1).default(0),
+}).min(1);
 
 module.exports = {
   createAssignmentSchema,
-  updateAssignmentSchema
+  updateAssignmentSchema,
 };
