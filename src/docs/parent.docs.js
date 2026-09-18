78659d8b89753f3092ecdca1d809e5d1d1f4411d
AbdulrhmanElshaer31 <abdelrhmanelshaer31gmail.com>
2026-09-18 20:05:05 +0300

    parent updated

--- src/docs/parent.docs.js
+++ src/docs/parent.docs.js
@@ -60,7 +60,7 @@
 
 /**
  * @swagger
- * /api/parent/phone:
+ * /api/parent:
  *   post:
  *     summary: Get parent token by parent phone
  *     description: Returns the parent_token associated with the given parent phone number
