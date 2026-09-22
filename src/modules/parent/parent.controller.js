5ea07b33bcc82fc6b2a3ec6152e0e19f81179b3b
AbdulrhmanElshaer31 <abdelrhmanelshaer31gmail.com>
2026-09-22 23:01:15 +0300

    test2

--- src/modules/parent/parent.controller.js
+++ src/modules/parent/parent.controller.js
@@ -22,7 +22,7 @@ const getPerentTokenByParentPhone = async (req, res, next) => {
       });
     }
 
-    const student = await parentService.getStudentByParentToken(token);
+    const student = await parentService.getStudentByParentToken(parent_token);
 
     if (!student) {
       return res.status(404).json({
