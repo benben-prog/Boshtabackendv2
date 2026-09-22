b9dbb0584a69ed60481d0d61a6ec46dfd3e80d6e
AbdulrhmanElshaer31 <abdelrhmanelshaer31gmail.com>
2026-09-22 22:53:08 +0300

    parent updated

--- src/modules/parent/parent.controller.js
+++ src/modules/parent/parent.controller.js
@@ -21,8 +21,11 @@ const getPerentTokenByParentPhone = async (req, res, next) => {
         message: "رقم الهاتف غير موجود برجاء متابعة السنتر!",
       });
     }
-    const student = parentService.getStudentByParentToken(parent_token);
-    const studentId = student.id;
+
+    const student = await parentService.getStudentByParentToken(parent_token);
+    const { id } = student;
+
+    // Fetch all data in parallel
     const [
       attendance,
       attendanceHistory,
@@ -33,19 +36,20 @@ const getPerentTokenByParentPhone = async (req, res, next) => {
       groupInfo,
       overallStats,
     ] = await Promise.all([
-      parentService.getParentDashboardAttendance(studentId),
-      parentService.getAttendanceHistory(studentId, 1),
-      parentService.getParentDashboardPayments(studentId),
-      parentService.getPaymentHistory(studentId, 1),
-      parentService.getAllExams(studentId),
-      parentService.getParentDashboardAssignments(studentId),
-      parentService.getGroupInfo(studentId),
-      parentService.getStudentOverallStats(studentId),
+      parentService.getParentDashboardAttendance(id),
+      parentService.getAttendanceHistory(id, 1),
+      parentService.getParentDashboardPayments(id),
+      parentService.getPaymentHistory(id, 1),
+      parentService.getAllExams(id),
+      parentService.getParentDashboardAssignments(id),
+      parentService.getGroupInfo(id),
+      parentService.getStudentOverallStats(id),
     ]);
 
     return res.status(200).json({
       success: true,
-      date: {
+      message: "تم تحميل البيانات بنجاح",
+      data: {
         student,
         attendance,
         attendanceHistory,
