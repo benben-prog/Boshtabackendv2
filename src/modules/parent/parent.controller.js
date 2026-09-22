7073fe39a188bb4a1cbe6a68e3b1a94bb4e3cb1c
AbdulrhmanElshaer31 <abdelrhmanelshaer31gmail.com>
2026-09-22 22:47:47 +0300

    parent updated

--- src/modules/parent/parent.controller.js
+++ src/modules/parent/parent.controller.js
@@ -21,7 +21,8 @@ const getPerentTokenByParentPhone = async (req, res, next) => {
         message: "رقم الهاتف غير موجود برجاء متابعة السنتر!",
       });
     }
-    const { id } = parentService.getStudentByParentToken(parent_token);
+    const student = parentService.getStudentByParentToken(parent_token);
+    const studentId = student.id;
     const [
       attendance,
       attendanceHistory,
@@ -32,19 +33,20 @@ const getPerentTokenByParentPhone = async (req, res, next) => {
       groupInfo,
       overallStats,
     ] = await Promise.all([
-      parentService.getParentDashboardAttendance(id),
-      parentService.getAttendanceHistory(id, 1),
-      parentService.getParentDashboardPayments(id),
-      parentService.getPaymentHistory(id, 1),
-      parentService.getAllExams(id),
-      parentService.getParentDashboardAssignments(id),
-      parentService.getGroupInfo(id),
-      parentService.getStudentOverallStats(id),
+      parentService.getParentDashboardAttendance(studentId),
+      parentService.getAttendanceHistory(studentId, 1),
+      parentService.getParentDashboardPayments(studentId),
+      parentService.getPaymentHistory(studentId, 1),
+      parentService.getAllExams(studentId),
+      parentService.getParentDashboardAssignments(studentId),
+      parentService.getGroupInfo(studentId),
+      parentService.getStudentOverallStats(studentId),
     ]);
 
     return res.status(200).json({
       success: true,
       date: {
+        student,
         attendance,
         attendanceHistory,
         payments,
